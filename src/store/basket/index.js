70963a39c7fe30e580e1adeba2bc4f62acf338f5
ivan <guiteache@gmail.com>
2023-05-28 15:34:19 +0300

    fixed detail refresh err

--- src/store/basket/index.js
+++ src/store/basket/index.js
@@ -31,7 +31,8 @@ class Basket extends StoreModule {
     if (!exist) {
       // Поиск товара в каталоге, чтобы его добавить в корзину.
       // @todo В реальном приложении будет запрос к АПИ вместо поиска по состоянию.
-      const item = this.store.getState().catalog.list.find(item => item._id === _id);
+      const listItem = this.store.getState().catalog.list.find(item => item._id === _id);
+      const item = listItem ? listItem : this.store.getState().product.product;
       list.push({...item, amount: 1}); // list уже новый, в него можно пушить.
       // Добавляем к сумме.
       sum += item.price;
